5704845e87d67d53dd1f86999c872b7e4b9e408c
eFko28 <josef.kovar28@gmail.com>
2024-10-06 19:02:19 +0200

    01_empty HOTOVO

--- 01_greetings_basics_empty.py
+++ 01_greetings_basics_empty.py
@@ -13,6 +13,8 @@ Na inputu jméno, příjmení. Na výstupu jeden ze 3 možných pozdravů včetn
 ### Jak vymazat terminál před opětovným spuštěním - cls pro Win, clear pro Unix-like systémy
 
 import os
+import random
+import datetime
 
 # Vymazání obrazovky terminálu (Windows)
 os.system("cls")
@@ -21,43 +23,36 @@ os.system("cls")
 ##############################################################
 ### Základní verze - vždy stejná odpověď
 
+#ziskani inputu
 jmeno = input("Jméno uživatele: ")
-prijmeni = input("Příjmení uživatele: ")
+prijmeni = input("Příjmení uživatele: ")           
 
-vystup = ("Ahoj " + jmeno + " " + prijmeni + "! Jak se dnes máš?")
-
-print("Základní verze bez obměny pozdravu")
+#prvni verze
+print("Základní verze bez obměny pozdravu:")
+vystup = ("Ahoj " + jmeno.title(jmeno) + " " + prijmeni.title(jmeno) + "! Jak se dnes máš?")
 print(vystup)
-print("________________")
+print("----------")
 
 
 ##############################################################
 ### Rozšířená verze - pseudonáhodný výběr bez zamíchání
 
-print("Rozšířená verze - pseudonáhodný výběr bez zamíchání")
+print("Rozšířená verze - pseudonáhodný výběr bez zamíchání:")
 
+#vytvoreni promenne s listem pozdravu
+pozdravy = [
+    f"Ahoj {jmeno.title()} {prijmeni.title()}! Jak se dnes máš?",
+    f"Ahojky! Jak se dnes {jmeno.title()} {prijmeni.title()} má?",
+    f"{jmeno.title()} {prijmeni.title()}! Těší mně!",
+    f"Přeji hezký den {jmeno.title()} {prijmeni.title()}",
+    f"Ahoj {prijmeni.title()}, jsi dnes {prijmeni.title()}",
+]
 
+#vyber nahodneho pozdravu do promenne random_greeting
+random_greeting = random.choice(pozdravy)
+print(random_greeting)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("----------")
 
 ##############################################################
 ### Rozšířená verze - random seed()
@@ -66,50 +61,39 @@ print("Rozšířená verze - pseudonáhodný výběr bez zamíchání")
 # side effect provedení v této části kódu má za následek i zamíchání volby při opětovném volání
 
 
+print("Rozšířená verze - random seed()")
+
+#vytvoreni seedu
+random.seed(50)
+
+#promenna pro nahodny pozdrav se seedem
+random_greeting_with_seed = random.choice(pozdravy)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(random_greeting_with_seed)
+print("----------")
 
 
 ##############################################################
 ### *verze - pozdrav podle denní doby
 
+print("Verze - pozdrav podle denní doby")
 
+#ziskani casu, vytvoreni promenne s presnosti na minuty
+current_daytime = datetime.datetime.now()
+minutes = current_daytime.minute
 
+#podminky pro output 
+if 0:00 <= minutes < 6:00:
+    output_time = "Tak brzo? Makej ještě spát!"
+elif 6:00 <= minutes < 11:30:
+    output_time = "Dobré ráno! Co budeš dneska programovat?"
+elif 11:30 <= minutes < 13:30:
+    output_time = "Už jsi byl na oběd? Nezapomínej, že musís krom programování i jíst!"
+elif 13:30 <= minutes < 21:00:
+    output_time = "Myslím, že by jsi měl ještě něco naprogramovat..."
+else:
+    output_time = "Padej spát! Ať máš zítra sílu na programování!"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(output_time)
+print("----------")
