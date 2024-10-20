0f98a558764635e26c184e9d6caf8e033ac6ebd8
eFko28 <josef.kovar28@gmail.com>
2024-10-20 19:28:02 +0200

    04, 05 HOTOVO

--- 04_random_numbers_basics_empty.py
+++ 04_random_numbers_basics_empty.py
@@ -28,7 +28,7 @@ os.system("cls")
 # fce generate_example, exercise_generator_simple, celková funkce example_generator_2numbers
 print("Ver1: Exercise generator")
 
-def generate_example() -> int:
+def generate_example() -> int:      #vytvoreni prikladu
     number1 = random.randint(1, 10)
     number2 = random.randint(1, 10)
 
@@ -42,43 +42,44 @@ def generate_example() -> int:
         result = number1 + number2
     else:
         result = number1 - number2
-    return number1, number2, operation, result
+    return number1, number2, operation, result #vrati vsechny hodnoty, se kteryma budu potom pracovat
 
 
 
 
-def exercise_generator_simple_2numbers():
+def exercise_generator_simple_2numbers(): #tato fce bezi az do konce(Q input od usr)
     while True:
-        try:
+        try:   
             number1, number2, operation, result = generate_example()
             operation = str(operation)
             if operation == "1":
                 operation = "*"
             elif operation == "2":
                 operation = "/"
-            elif operation == "3":
+            elif operation == "3":         #prevedeni hodnot pod var opration na stringy
                 operation = "+"
             elif operation == "4":
                 operation = "-"
 
-            print(f"Exercise: {number1} {operation} {number2}")
-            user_result = input("Your answer: (or enter Q to end the programm)")
-            if user_result == "Q":
+            print(f"Exercise: {number1} {operation} {number2}")    #vypsani prikladu usr
+            user_result = input("Your answer: (or enter Q to end the program) ")  #input vysledku
+            if user_result == "Q":   #pokud Q, rozbije loop a ukonci tedy program
                 print("Quitting the exercise generator")
+                break
             elif user_result == "q":
                 print("Quitting the exercise generator")
                 break
             user_result = float(user_result)
-            if user_result == result:
+            if user_result == result:   #porovnani spravneho vysledku
                 print("Thats right, generating another exercise")
             elif user_result != result:
                 print(f"That wasnt right! The correct result was: {result}")
                 print("Generating another exercise.")
-        except ValueError:
+        except ValueError:    #osetreni vstupu
             print("Non-numerical input. Generating a new exercise.")
 
 
-print("The programm will generate a random exercise. You can either answer it or enter Q to quit.")
+print("The program will generate a random exercise. You can either answer it or enter Q to quit.")
 exercise_generator_simple_2numbers()
 
     
@@ -86,97 +87,81 @@ exercise_generator_simple_2numbers()
 ##############################################################
 ### Generátor příkladů - n čísel a operace mezi nimi (+, -), přidána funkce statistika
 # funkce numbers_generator, example_generator_advance, user_statistics, celková fce example_generator_processor
-print("Ver2: stats, n numbers")
+print("\nVer2: stats, n numbers")
 
-def create_variables() -> list:
+def create_variables() -> list:    #vytvoreni listu x hodnot (dle usr inputu) pro priklady
     while True:
         try:
-            term_count = int(input("How many terms would you like to have to exercise with? (2-10)"))
+            term_count = int(input("How many terms would you like to have to exercise with? (2-10) "))
             if 2 <= term_count <= 10:
                 variables = []
-                for i in range(1, term_count + 1):
+                for i in range(term_count):      #vytvoreni listu, delka dle poskytnuteho inputu
                     variables.append(random.randint(1, 10))
                 return variables
+
             elif term_count < 2 or term_count > 10:
-                print("Incorrect input. Must input whole numbers 2-10.")
+                print("Incorrect input. Must input whole numbers 2-10.")  #ostreni vstupu, jen cisla ktera davaji smysl
               
-        except ValueError:
-            print("Incorrect input. Must input whole numbers 2-10.")       
-        
-def create_operations(term_count) -> dict:
+        except ValueError:     #osetreni vstupu, pouze cisla
+            print("Incorrect input. Must input whole numbers 2-10.")
+
+
+def create_operations(term_count: int) -> list:  #vytvoreni listu operaci, uz potrebujeme zadat do funkce promennou -> delka listu
+    operations_list_random = []
+    operations_list = ["+", "-", "*", "/"]
+    for i in range(1, term_count + 1):   #kvuli generalizaci davame +1
+        operations_list_random.append(random.choice(operations_list))
+    return operations_list_random  #vrati hodnoty
+
+def generate_example() -> str:   #vytvoreni prikladu
+    variables = create_variables()    #uziti definiovanych funkci
+    operations = create_operations(len(variables) - 1)    # -1, protoze potrebujeme o 1 vice cisel nez operaci
+    example = ""    #vytvoreni str example
+    for i in range(len(variables) - 1):    #prida do str example vzdy 1. variables, 1. operations, 2. 2., 3. 3. atd, dle delky variables
+        example += str(variables[i]) + " " + operations[i] + " "
+    example += str(variables[-1])    #dodani posledni ciselne promenne (cisel o 1 vic nez operaci)
+    return example #vrati str pro fci eval()
+
+def evaluate_example(example: str) -> float:   #vyhodnoti priklad, str -> float
+    try:
+        return round(eval(example), 4)
+    except ZeroDivisionError:       #pokud neexistuje reseni kvuli deleni 0, nevrati nic
+        return None
+    
+
+def exercise_generator_n_numbers():   #finalni fce - vsecky predtim definovane spoji do finalniho programu
+    correct_answers = 0      #definice promennych pro statistiky
+    wrong_answers = 0
+    total_answers = 0
     while True:
-        try:
-            if 2 <= term_count <= 10:
-                variables = {}
-                for i in range(term_count):
-                    operation = f'operation{i + 1}'
-                    
-                    
-                return variables
-            elif term_count < 2 or term_count > 10:
-                print("Incorrect input. Must input whole numbers 2-10.")
-              
-        except ValueError:
-            print("Incorrect input. Must input whole numbers 2-10.")     
+        example = generate_example()   #vytvori hodnoty, vyhodnoti je a pokud nebude existovat hodnota z fce evaluate_example (deleni nulou)
+        result = evaluate_example(example)          #provede continue -> bude donekonecna vytvaret hodnoty, dokud jedna nebude existovat
+        if result is None:
+            continue
+        print(f"Expression to solve: {example} (or Q to quit the program.)")     #podnet pro usr
+        while True:
+            try:
+                user_result = input("")   #input vysledku od usr
+                total_answers += 1
+                if user_result.lower() == "q":  #pokud Q, rozbije program
+                    print("Quitting the exercise generator. Hope you liked it!")
+                    print(f"\nStatistics: Correct answers: {correct_answers}, Wrong answers: {wrong_answers}, Total answers: {total_answers}")
+                    return    #zde return, protoze tehdy ukonci python celou fci (pokud neco vrati, nemuze dal probihat)
+                user_result = float(user_result) #prevedeni na float
+                if round(user_result, 4) == result:  #porovnani usr inputu s presnosti na 4 desetinne mista
+                    print("Thats right! Generating another exercise:")
+                    correct_answers += 1
+                    break
+                elif round(user_result, 4) != result:
+                    print(f"That wasnt right! The correct result was: {result}")
+                    wrong_answers += 1
+                    break
 
+            except ValueError:  #osetreni vstupu
+                print("Incorrect input. Must be numbers (or Q for quitting).")
 
-
-
-
-
-
-def generate_example() -> int:
-    numbers = create_variables()
-
-    operation = random.randint(1, 4)
-
-    if operation == 1:
-        result = 
-    elif operation == 2:
-        result = number1 / number2
-    elif operation == 3:
-        result = number1 + number2
-    else:
-        result = number1 - number2
-    return numbers, operation, result
-
-
-
-
-def exercise_generator_simple_2numbers():
-    while True:
-        try:
-            numbers, operation, result = generate_example()
-            operation = str(operation)
-            if operation == "1":
-                operation = "*"
-            elif operation == "2":
-                operation = "/"
-            elif operation == "3":
-                operation = "+"
-            elif operation == "4":
-                operation = "-"
-                
-            print(f"Exercise: {number1} {operation} {number2}")
-            user_result = input("Your answer: (or enter Q to end the programm)")
-            if user_result == "Q":
-                print("Quitting the exercise generator")
-            elif user_result == "q":
-                print("Quitting the exercise generator")
-                break
-            user_result = float(user_result)
-            if user_result == result:
-                print("Thats right, generating another exercise")
-            elif user_result != result:
-                print(f"That wasnt right! The correct result was: {result}")
-                print("Generating another exercise.")
-        except ValueError:
-            print("Non-numerical input. Generating a new exercise.")
-
-
-print("The programm will generate a random exercise. You can either answer it or enter Q to quit.")
-exercise_generator_simple_2numbers()
-
+print("The program will now also ask with how many terms you wish to practice with. You can either answer or input Q to quit like before.")
+exercise_generator_n_numbers()
 
 
 
--- 05_en_de_cryption_ASCII_empty.py
+++ 05_en_de_cryption_ASCII_empty.py
@@ -11,11 +11,19 @@
 """
 
 import os
+os.system("cls")
 
 
 ##############################################################
 ### Vypiš ASCII kódy DEC / CHAR od 1 do 127
 # funkce ascii_table
+def ascii_table():
+    print("ASCII PŘEVODNÍK - DEC/CHAR")       #uvod
+    print("Dec -- Char")       
+
+    for i in range(1, 128):    #loop, ktery vypise hodnotu char pro i a i jako takove (i = decimal)
+        char = chr(i)
+        print(f"{i:4}  {char:5}")   #:4 a :5 dela prostor -> zabere tolik mist, pokud ma min, napise se co nejvic v pravo
 
 
 
@@ -26,13 +34,36 @@ import os
 
 
 
+def ascii_table_with_range(start=32, end=127):
+    print("ASCII CONVERTER - DEC/CHAR/BIN/OCT/HEX")
+    print(f"Characters from {start} to {end}:")
+    print(f"{'Dec':<5}{'Char':<5}{'Bin':<10}{'Oct':<5}{'Hex':<5}")
+    
+    for i in range(start, end + 1):       #vypise vsechny korespondujici hodnoty pro decimal od 32 do 127
+        print(f"{i:<5}{chr(i):<5}{bin(i)[2:]:<10}{oct(i)[2:]:<5}{hex(i)[2:].upper():<5}")
+
+
 
 
 ##############################################################
 ### Preveď znak na ASCII=DEC hodnotu a poté vrať hodnotu znaku v BIN/OCT/HEX podle base
 # funkce char_to_base
 # využito v main
+def ascii_convert_char_to_base(char: str, return_type: str) -> str:
+    if len(char) != 1:    #osetreni vstupu na pouze 1-mistny
+        raise ValueError()
 
+    numerical_value = ord(char) #prevede jakykoliv vstup na ASCII decimal
+    
+    if return_type.upper() == "BIN":     #podle usr specifikovaneho return_type vraci hodnoty korespondujici druhum zapisu
+        return bin(numerical_value)
+    elif return_type.upper() == "OCT":
+        return oct(numerical_value)    
+    elif return_type.upper() == "HEX":
+        return hex(numerical_value)
+    
+    else:
+        raise ValueError("Invalid return type. Use 'oct', 'bin', or 'hex'.")     #ostreni vstupu
 
 
 
@@ -41,12 +72,36 @@ import os
 # Možnost zvolit více sloupečků, default 1
 # funkce ascii_table_multicolumn
 
+def ascii_table_multicolumn(start: int, end: int, columns: int = 1) -> None:     #definujeme 3 promenne - start, end, columns(pro zatim konstanta)
+    if start < 1 or start > 126:   #osetreni vstupu
+        raise ValueError("Invalid start value. Must be 1-126.")
+    if end < 2 or end > 127:
+        raise ValueError("Invalid end value. Must be 2-127.")
+    output = []    #vytvoreni listu output
+    for i in range(start, end):    #pokud je hodnota 1-32, ulozi char jako "nevytisknutelny charakter"; pokud je 32-127, ulozi char jako korespondujici charakter
+        if i >= 32:
+            char = chr(i)
+        elif i <= 31:
+            char = "Non-printable character"
+        output.append(f"Char: {char} -- Dec: {i} -- Bin: {bin(i)[2:]} -- Oct: {oct(i)[2:]} -- Hex: {hex(i)[2:]}") #prida k listu spravne hodnoty i-krat
+
+    print(f"{'Char':<5}{'Dec':<5}{'Bin':<5}{'Oct':<5}{'Hex':<5}  " * columns)   #vytiskne columns-krat (zatim 1) zahlavi k outputu
+    for i in range(start, end + 1, columns):   #venkovni loop: definuje vychozi bod pro kazdy radek
+        for index in range(i, min(i + columns, end + 1)):   #vnitrni loop: vypisuje hodnoty korespondujici s i z venkovniho loopu
+            print(f"{chr(index):<5}{index:<5}{bin(index)[2:]:<5}{oct(index)[2:]:<5}{hex(index)[2:].upper():<5}")
+    print("\n")    #pokazde napise novy radek - pekny zapis
+
+print("Hey! I will output you an ASCII codes for a specific range of ASCII codes you wish to have output.")   #uziti fce nakonec
+start = int(input("Range from: (Decimal input) "))
+end = int(input("Range to: (Decimal input) "))
+ascii_table_multicolumn(start, end)
+
 
 
 
 ##############################################################
 ### Spuštění programu - MAIN
-
+"""
 if __name__ == "__main__":
     os.system("cls")
 
@@ -62,3 +117,4 @@ if __name__ == "__main__":
     print("------------------------------------------------\n")
 
     ascii_table_multicolumn(35,62,4)
+"""
